ad59fdbfc899eee4048e1afbea6e7af5f54f6593
Michal Hrbek <michal@dee.cz>
2023-09-18 22:20:53 +0200

    Updated url and hashes for Albert

--- ItemAlbert.py
+++ ItemAlbert.py
@@ -1,13 +1,15 @@
 from ItemBase import ItemBase
 
 class ItemAlbert(ItemBase):
-	url: str # https://www.albertdomuzdarma.cz{url}
-	price_label: str
+	url: str # https://www.albert.cz{url}
+	unit_label: str
+	ammount: str
 	net_content: str
 	def __init__(self, item, category: str) -> None:
 		self.name = item["name"]
 		self.category = category
 		self.price = item["price"]["value"]
-		self.price_label = item["price"]["supplementaryPriceLabel1"]
+		self.unit_label = item["price"]["supplementaryPriceLabel1"]
+		self.ammount = item["price"]["supplementaryPriceLabel2"]
 		self.store = "albert"
 		self.url = item["url"]
--- ScraperAlbert.py
+++ ScraperAlbert.py
@@ -6,12 +6,13 @@ from math import ceil
 
 class ScraperAlbert(ScraperBase): # Scan takes 150s
 	def scrape() -> list[ItemAlbert]:
-		categories = requests.get('https://www.albertdomuzdarma.cz/api/v1/?operationName=LeftHandNavigationBar&variables={"rootCategoryCode":"","cutOffLevel":"1","lang":"cs"}&extensions={"persistedQuery":{"version":1,"sha256Hash":"29a05b50daa7ab7686d28bf2340457e2a31e1a9e4d79db611fcee435536ee01c"}}').json()["data"]["leftHandNavigationBar"]["levelInfo"]
+		categories = requests.get('https://www.albert.cz/api/v1/?operationName=LeftHandNavigationBar&variables={"rootCategoryCode":"","cutOffLevel":"1","lang":"cs"}&extensions={"persistedQuery":{"version":1,"sha256Hash":"29a05b50daa7ab7686d28bf2340457e2a31e1a9e4d79db611fcee435536ee01c"}}').json()["data"]["leftHandNavigationBar"]["levelInfo"]
 		out_list = []
 		try:
 			for i in tqdm(categories):
 				for j in range(ceil(i["productCount"]/50)):
-					resp = requests.get('https://www.albertdomuzdarma.cz/api/v1/?operationName=GetCategoryProductSearch&variables={"lang":"cs","category":"' + i["code"] + '","pageNumber":' + str(j) + ',"pageSize":20,"filterFlag":true,"plainChildCategories":true}&extensions={"persistedQuery":{"version":1,"sha256Hash":"65b0b1aacb2caeea6468873e8f7fde0ef82ffb0c4f9c93583c41070fa1f13c82"}}').json()
+					# I have no idea how persistedQuery works. Some hash is required but you can still change the search parameters. There's no query param to hash so idk where to get the hash
+					resp = requests.get('https://www.albert.cz/api/v1/?operationName=GetCategoryProductSearch&variables={"lang":"cs","category":"' + i["code"] + '","pageNumber":' + str(j) + ',"pageSize":20,"filterFlag":true,"plainChildCategories":true}&extensions={"persistedQuery":{"version":1,"sha256Hash":"25fdff69c2396b20f500d39cc3e5967a066ee90eb8678e1e264575dfb9433060"}}').json()
 					for k in resp["data"]["categoryProductSearch"]["products"]:
 						out_list.append(ItemAlbert(k, i["name"]))
 		except KeyboardInterrupt:
