068a48089d770241af4b6bc02678a68c7475949a
Michal Hrbek <michal@dee.cz>
2023-10-24 17:01:43 +0200

    Updated hash

--- ScraperAlbert.py
+++ ScraperAlbert.py
@@ -14,7 +14,7 @@ class ScraperAlbert(ScraperBase): # Scan takes 150s
 				category_url = i["url"].split('/')[2]
 				for j in range(ceil(i["productCount"]/50)):
 					# I have no idea how persistedQuery works. Some hash is required but you can still change the search parameters. There's no query param to hash so idk where to get the hash
-					resp = requests.get('https://www.albert.cz/api/v1/?operationName=GetCategoryProductSearch&variables={"lang":"cs","category":"' + i["code"] + '","pageNumber":' + str(j) + ',"pageSize":20,"filterFlag":true,"plainChildCategories":true}&extensions={"persistedQuery":{"version":1,"sha256Hash":"25fdff69c2396b20f500d39cc3e5967a066ee90eb8678e1e264575dfb9433060"}}').json()
+					resp = requests.get('https://www.albert.cz/api/v1/?operationName=GetCategoryProductSearch&variables={"lang":"cs","category":"' + i["code"] + '","pageNumber":' + str(j) + ',"pageSize":20,"filterFlag":true,"plainChildCategories":true}&extensions={"persistedQuery":{"version":1,"sha256Hash":"1472b107dfda8f999d13674f79bb9a2ece228c5e450e78821ad1c2d5188647c8"}}').json()
 					for k in resp["data"]["categoryProductSearch"]["products"]:
 						if category_url == k["url"].split('/')[2]: # This is to exclude duplicates. Tell me if you know of a better way to do this
 							out_list.append(ItemAlbert(k, category_name))
