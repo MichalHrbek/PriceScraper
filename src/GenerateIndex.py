bd02924ec17ad8c15fabf7d76a0334ccf1154a6d
Michal Hrbek <michal@dee.cz>
2024-10-27 20:25:52 +0100

    Faster indexing

--- src/GenerateIndex.py
+++ src/GenerateIndex.py
@@ -1,5 +1,5 @@
-import json, glob
-from DataManager import get_current_all
+import json, glob, linecache
+from DataManager import get_current_all, PROPS
 
 def item_to_str(item) -> str:
 	return f'{item["name"]}, {item["price"]} CZK, {item["store"]}'
@@ -7,7 +7,7 @@ def item_to_str(item) -> str:
 def item_to_path(item) -> str:
 	return f'{item["store"]}/{item["id"]}.csv'
 
-def generate_index():
+def generate_index_json():
 	c = get_current_all()
 	d = {}
 	d["items"] = {item_to_path(i): {"name": item_to_str(i), "store": i["store"]} for i in c}
@@ -16,5 +16,14 @@ def generate_index():
 	with open("out/index.json", "w") as f:
 		f.write(json.dumps(d))
 
+def generate_index_csv():
+	stores = glob.glob("out/*/")
+	stores.remove("out/error/")
+	with open("out/index.csv", "w") as f:
+		f.write(','.join(PROPS) + "\n")
+		for i in stores:
+			for j in glob.glob(i + "*"):
+				f.write(linecache.getline(j, 2))
+
 if __name__ == "__main__":
-	generate_index()
+	generate_index_json()
